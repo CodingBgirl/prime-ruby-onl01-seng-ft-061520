f6af89188044455e41dc065200aa981c630d3585
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-23 22:06:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,9 @@
 #A prime number is an integer, or whole number, that has only two factors — 1 and itself
 
 def prime?(number)
-  
+  counter = 2
+  while counter <= number - 1
+    if counter % number == 0
+      return false
+  end
 end
